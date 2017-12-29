aab4957498ac1d1faf4ddf1c29303f4b1cb1ab2a
Brik Royster <elkemore@gmail.com>
2017-12-29 12:05:46 -0700

    better use of node, starcolor ui, and object builder started

--- electron/src/js/index.js
+++ electron/src/js/index.js
@@ -16,4 +16,65 @@ $(document).ready(function() {
 
   $('#file-c').val(120)
   $('#file-d').val(74)
+  $('#star-color').colorpicker({
+    format: 'rgb'
+  })
+
+  $('#patch-button').click(function() {
+    json = getPatchJson()
+    console.log(json)
+  })
 })
+
+// helper to get selected option's value in select
+function selected(id) {
+  return $('#' + id + ' option:selected').val()
+}
+
+// helper to return whether or not a checkbox is checked
+function checked(id) {
+  return $('#' + id).is(':checked')
+}
+
+// helper to get value of selected radio option
+function radio(name) {
+  return $('input[name=' + name + ']:checked').val()
+}
+
+// converts RGB color to 5551 hex
+function toFiveBit(color) {
+  rgba = 0
+  rgba += (color.r / 8) << 11
+  rgba += (color.g / 8) << 6
+  rgba += (color.b / 8) << 1
+  rgba += 1 // alpha
+  
+  return rgba
+}
+
+function getPatchJson() {
+  return {
+    fileA: selected('file-a'),
+    fileB: selected('file-b'),
+    fileC: selected('file-c'),
+    fileD: selected('file-d'),
+    infiniteLives: checked('infinite-lives'),
+    levelReset: checked('level-reset'),
+    starSelect: checked('star-select'),
+    thiBehavior: radio('thi-behavior'),
+    savestates: checked('savestates'),
+    levelSelect: checked('level-select'),
+    timer: checked('timer'),
+    timerInCastle: checked('timer-in-castle'),
+    timerCentiseconds: checked('timer-centiseconds'),
+    showTimer: radio('show-timer'),
+    stopTimer: radio('stop-timer'),
+    lagCounter: checked('lag-counter'),
+    lagAsLives: checked('lag-as-lives'),
+    speedDisplay: checked('speed-display'),
+    speedAsStars: checked('speed-as-stars'),
+    music: checked('music'),
+    unobtainedStars: checked('unobtained-stars'),
+    starColor: toFiveBit($('#star-color').data('colorpicker').color.toRGB())
+  }
+}
